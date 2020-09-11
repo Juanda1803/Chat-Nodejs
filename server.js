d05330280aecebb82c81c20c9bbdff6ff71f9935
Juanda1803 <j3045771061@gmail.com>
2020-09-11 13:35:17 -0500

    [ADD] body and query

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
 
 let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);
 
-app.use("/", function (req, res) {
-  res.send("Hello");
+router.get("/message", function (req, res) {
+  res.send("list of message");
+});
+
+router.delete("/message", function (req, res) {
+  console.log(req.body);
+  console.log(req.query);
+  res.send(`Message ${req.body.text} added succesful`);
 });
 
 app.listen(3000);
-console.log("The app this listened in http://localhost:3000");
+console.log("The app is listening on http://localhost:3000");
