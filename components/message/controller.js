da282e9b7918675270ae77305323f04c6347be9a
Juanda1803 <j3045771061@gmail.com>
2020-10-09 09:58:24 -0500

    [ADD] Images

--- components/message/controller.js
+++ components/message/controller.js
@@ -1,58 +1,73 @@
-const store = require("./store");
+const store = require('./store');
 
-const addMessage = (user, messsage) => {
-  return new Promise((resolve, reject) => {
-    if (!user || !messsage) {
-      console.error("[messageController] No user or message");
-      return reject("The data is wrong");
-    }
-    const fullMessage = {
-      user: user,
-      messsage: messsage,
-      date: new Date(),
-    };
-    store.add(fullMessage);
+function addMessage(chat, user, message, file) {
+    return new Promise((resolve, reject) => {
+        if (!chat || !user || !message) {
+            console.error('[messageController] No hay chat usuario o mensaje');
+            reject('Los datos son incorrectos');
+            return false;
+        }
 
-    resolve(fullMessage);
-  });
-};
+        let fileUrl = '';
+        if (file) {
+            fileUrl = 'http://localhost:3000/app/files/' + file.filename;
+        }
 
-const getMessage = (filterUser) => {
-  return new Promise((resolve, reject) => {
-    resolve(store.list(filterUser));
-  });
-};
+        const fullMessage = {
+            chat: chat,
+            user: user,
+            message: message,
+            date: new Date(),
+            file: fileUrl,
+        };
+    
+        store.add(fullMessage);
 
-const updateMessage = (id, message) => {
-  return new Promise(async (resolve, reject) => {
-    if (!id || !message) {
-      reject("Invalid data");
-      return false;
-    }
-    const result = await store.updateText(id, message);
-    resolve(result);
-  });
-};
+        resolve(fullMessage);
+    });
+}
 
-const deleteMessage = (id) => {
-  return new Promise((resolve, reject) => {
-    if (!id) {
-      reject("Id invalid");
-    }
-    store
-      .remove(id)
-      .then(() => {
-        resolve();
-      })
-      .catch((e) => {
-        reject(e);
-      });
-  });
-};
+function getMessages(filterChat) {
+    return new Promise((resolve, reject) => {
+        resolve(store.list(filterChat));
+    })
+}
+
+function updateMessage(id, message) {
+    return new Promise(async (resolve, reject) => {
+        console.log(id);
+        console.log(message);
+        if (!id || !message) {
+            reject('Invalid data');
+            return false;
+        }
+
+        const result = await store.updateText(id, message);
+
+        resolve(result);
+    })
+}
+
+function deleteMessage(id) {
+    return new Promise((resolve, reject) => {
+        if (!id) {
+            reject('Id invalido');
+            return false;
+        }
+
+        store.remove(id)
+            .then(() => {
+                resolve();
+            })
+            .catch(e => {
+                reject(e);
+            })
+    });
+}
 
 module.exports = {
-  addMessage,
-  getMessage,
-  updateMessage,
-  deleteMessage,
+    addMessage,
+    getMessages,
+    updateMessage,
+    deleteMessage,
 };
--- components/message/network.js
+++ components/message/network.js
@@ -1,59 +1,51 @@
-const express = require("express");
+const express = require('express');
+const multer = require('multer');
 
-const controller = require("./controller");
+const response = require('../../network/response');
+const controller = require('./controller');
 const router = express.Router();
-const response = require("../../network/response");
-const e = require("express");
 
-router.get("/", function (req, res) {
-  const filterMessages = req.query.user || null;
-  controller
-    .getMessage(filterMessages)
-    .then((messageList) => {
-      response.success(req, res, messageList, 200);
-    })
-    .catch((e) => {
-      response.error(req, res, "Unexpected Error", 500, e);
-    });
+const upload = multer({
+    dest: 'public/files/',
 });
 
-router.post("/", function (req, res) {
-  controller
-    .addMessage(req.body.user, req.body.message)
-    .then((fullMessage) => {
-      response.success(req, res, fullMessage, 201);
-    })
-    .catch((e) => {
-      response.error(
-        req,
-        res,
-        "Unexpected error",
-        400,
-        "Error in the controller"
-      );
-    });
+router.get('/', function (req, res) {
+    const filterMessages = req.query.chat || null;
+    controller.getMessages(filterMessages)
+        .then((messageList) => {
+            response.success(req, res, messageList, 200);
+        })
+        .catch(e => {
+            response.error(req, res, 'Unexpected Error', 500, e);
+        })
+});
+router.post('/', upload.single('file'), function (req, res) { 
+    controller.addMessage(req.body.chat, req.body.user, req.body.message, req.file)
+        .then((fullMessage) => {
+            response.success(req, res, fullMessage, 201);    
+        })
+        .catch(e => {
+            response.error(req, res, 'Informacion invalida', 400, 'Error en el controlaor');
+        });
+});
+router.patch('/:id', function (req, res) {
+    controller.updateMessage(req.params.id, req.body.message)
+        .then((data) => {
+            response.success(req, res, data, 200);
+        })
+        .catch(e => {
+            response.error(req, res, 'Error interno', 500, e);
+        });
 });
 
-router.patch("/:id", (req, res) => {
-  controller
-    .updateMessage(req.params.id, req.body.message)
-    .then((data) => {
-      response.success(req, res, data, 200);
-    })
-    .catch((e) => {
-      response.error(req, res, "Internal error ", 500, e);
-    });
-});
-
-router.delete("/:id", (req, res) => {
-  controller
-    .deleteMessage(req.params.id)
-    .then(() => {
-      response.success(req, res, `User ${req.params.id} deleted`, 200);
-    })
-    .catch((e) => {
-      response.error(req, res, "Internal error", 500, e);
-    });
+router.delete('/:id', function(req, res) {
+    controller.deleteMessage(req.params.id)
+        .then(() => {
+            response.success(req, res, `Mensaje ${req.params.id} eliminado`, 200);
+        })
+        .catch(e => {
+            response.error(req, res, 'Error interno', 500, e);
+        });
 });
 
 module.exports = router;
